a1288fdcc7e20c97a01a549db40df0ecc4d3619e
Daniel Moster <d.moster@icloud.com>
2019-11-28 02:04:19 -0500

    add Font Awesome and jQuery files

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.render('pages/index'))
+app.get('/about', (req, res) => res.render('pages/about'))
 app.post('/profile', logIn)
 
 app.listen(app.get('port'), () => {
