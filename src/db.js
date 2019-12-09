f57b2a31b09c3a13294ad7c246caa2678b19956b
Daniel Moster <22249073+dmoster@users.noreply.github.com>
2019-12-09 18:33:39 +0000

    modularize application

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg')
-const connectionString = process.env.DATABA
+const connectionString = process.env.DATABASE_URL
 const pool = new Pool({ connectionString: connectionString })
 
 
