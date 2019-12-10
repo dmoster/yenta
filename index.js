ef51f3b3423a729b407f0b303b0af34e8c989742
Daniel Moster <d.moster@icloud.com>
2019-12-10 07:55:18 -0500

    update for merge

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const saltRounds = 10
 
 const { Pool } = require('pg')
 // BEFORE COMMIT: Remove alternate
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL || 'postgres://qezuaezksgohlm:a966bad868a94a25c5b4c986001f6dc5415fe236f0cb3a816e295229c11a0b9c@ec2-107-21-98-89.compute-1.amazonaws.com:5432/dqmg3u7e4g6l8?ssl=true'
 const pool = new Pool({ connectionString: connectionString })
 
 app.set('port', (process.env.PORT || 5000))
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'b7jms-7jtb7ctc-a7jb&s-7fb', //process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
